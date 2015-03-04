a6aee31adb16e424ff52471e4f30dd270dab7a46
torreswoo <dew1405@hanmail.net>
2015-03-04 23:44:16 +0900

    first commit

---
+++ testJS_jQuery01.js
@@ -0,0 +1,14 @@
+//<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+//<script type="text/javascript" src="testJS.js"></script>
+
+
+$(document).ready( function(){
+
+    $('h1').hover(function(), )
+
+}
+
+
+
+
+);
