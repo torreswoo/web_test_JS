76f7c670ebd4ff4d11c2b417854146feb4fedb3e
torreswoo <dew1405@hanmail.net>
2015-03-04 23:54:58 +0900

    test study

--- testJS.js
+++ testJS.js
@@ -1,20 +1,14 @@
-//<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-//<script type="text/javascript" src="testJS.js"></script>
+function sum(){
 
-$(document).ready(function() {
-    var array = [
-        { name: 'Google', link: 'http://www.google.co.kr' },
-        { name: 'Naver', link: 'http://www.naver.com' },
-        { name: 'Daum', link: 'http://www.daum.net' },
-    ];
-        
-    $.each(array, function (index, item) {
-        var output = '';
-            
-        output += '<a href="' + item.link + '">';
-        output += '<h1>' + item.name + '</h1>';
-        output += '</a>';
-            
-        document.body.innerHTML += output;
-    });
-});
+    var result = 0;
+
+    for (var i = 0; i < arguments.length; i++) {
+        var tmp = arguments[i]
+
+        if(isNaN(tmp)){
+            throw new Error('지정값이 숫자가 아닙니다.: '+tmp);
+        }
+        result += tmp;
+    };
+    return result;
+}
