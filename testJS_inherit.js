841ef44c4fd2b5576bc5b132cb1ffdfd98a34c7f
torreswoo <dew1405@hanmail.net>
2015-03-05 23:05:44 +0900

    JS OOP inherit complete

--- testJS_inherit.js
+++ testJS_inherit.js
@@ -22,3 +22,16 @@ Member.prototype.getName = function(){
 };
 
 //SpecialMember class extends Member
+var SpecialMember = function(firstName, lastName, role){
+    initializeBase(this, Member, [firstName, lastName]);
+    this.role = role;
+}
+
+SpecialMember.prototype.isAdministrator = function(){
+    return (this.role =='Administrator');
+}
+
+//test-----------------------
+var mem = new SpecialMember('torres', 'woo', 'Administrator');
+document.writeln( 'name: '+mem.getName() );
+document.writeln( 'admin : '+mem.isAdministrator());
